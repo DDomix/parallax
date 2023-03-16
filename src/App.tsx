12cb3c1e8b39511cf935cb35914b3bdffda652c3
DDomix <dominik.papp1010@gmail.com>
2023-03-16 16:11:43 +0100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Parallax } from 'react-parallax';
+import kep1 from '../src/img/1.jpg';
+import kep2 from '../src/img/2.jpg';
+import kep3 from '../src/img/3.jpg';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Parallax strength={600} bgImage={kep1}>
+        <div className='content'>
+          <div className='text-content'>ASD</div>
+        </div>
+      </Parallax>
+      <Parallax strength={600} bgImage={kep2}>
+        <div className='content'>
+          <div className='text-content'>ASD</div>
+        </div>
+      </Parallax>
+      <Parallax strength={600} bgImage={kep3}>
+        <div className='content'>
+          <div className='text-content'>ASD</div>
+        </div>
+      </Parallax>
     </div>
   );
 }
